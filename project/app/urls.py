c5ffa59cceff5f53ac7c3b9fc9ba2cd9054b0959
Bogan16 <bogdan66334@gmail.com>
2023-08-30 17:50:11 +0300

    Fixed the erors in urls project

--- project/app/urls.py
+++ project/app/urls.py
@@ -1,21 +1,11 @@
-# from django.urls import path
-
-# from . import views
-
-# urlpatterns = [
-#     path('movies/', views.movies),
-#     path('movies/<int:movie_id>', views.movie),
-#     # path('movies/<int:arg>', views.movie)
-# ]
-
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('reviews/', views.reviews),
     path('reviews/<int:review_id>/', views.review_detail),
-    path('books/', views.books),  # Новый URL для представления книг
-    path('books/<int:book_id>/', views.book_detail),  # Новый URL для конкретной книги
+    path('books/', views.books),
+    path('books/<int:book_id>/', views.book_detail),
     path('movies/', views.movies),
     path('movies/<int:movie_id>/', views.movie),
 ]
